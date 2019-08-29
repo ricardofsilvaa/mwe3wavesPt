8532b2281983749a1699e9265bfa4525346a49a7
Ricardo F. Silva <31420081+ricardofsilvaa@users.noreply.github.com>
2019-08-29 14:27:07 -0300

    Pt-BR

--- src/components/app.js
+++ src/components/app.js
@@ -25,7 +25,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="container">
-    		    <input className="btn btn-primary" type="submit" value="Auth" onClick={this.authFunc}/>
+    		    <input className="btn btn-primary" type="submit" value="Autentificação" onClick={this.authFunc}/>
     	    </div>
         )
     }
